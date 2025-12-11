ed01ad862d79b697e8bc3bdfc4e977b3853be17b
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2025-12-11 07:24:56 +0545

    retrieve cookies information

---
+++ src/QuestionEighteen.java
@@ -0,0 +1,17 @@
+//Write a program to retrieve cookies information.
+
+import java.net.*;
+import java.util.List;
+
+public class QuestionEighteen {
+    public static void main(String[] args) throws Exception {
+        CookieHandler.setDefault(new CookieManager());
+        URL url = new URL("https://www.facebook.com");
+        URLConnection conn = url.openConnection();
+        conn.getContent();
+        List<HttpCookie> cookies = ((CookieManager) CookieHandler.getDefault()).getCookieStore().getCookies();
+        for(HttpCookie cookie : cookies){
+            System.out.println("Cookie : "+cookie.getName()+ " = "+cookie.getValue());
+        }
+    }
+}

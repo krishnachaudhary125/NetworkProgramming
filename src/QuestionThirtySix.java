3879635e728320e2bfb2fa9cca1caf87b1c9c008
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-04-05 20:05:59 +0545

    program to get information about the UDP socket.

--- src/QuestionThirtySix.java
+++ src/QuestionThirtySix.java
@@ -1,3 +1,4 @@
+//Write a program to get information about the UDP socket.
 import java.net.DatagramSocket;
 
 public class QuestionThirtySix {
@@ -6,7 +7,7 @@ public class QuestionThirtySix {
         System.out.println("Local Address : " + socket.getLocalAddress());
         System.out.println("Local Port    : " + socket.getLocalPort());
         System.out.println("Send Buffer   : " + socket.getSendBufferSize());
-        System.out.println("Recv Buffer   : " + socket.getReceiveBufferSize());
+        System.out.println("Receive Buffer   : " + socket.getReceiveBufferSize());
         System.out.println("Timeout       : " + socket.getSoTimeout());
         System.out.println("Broadcast     : " + socket.getBroadcast());
         socket.close();

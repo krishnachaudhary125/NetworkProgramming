78ecddba2963ba4a000773ee09bfb84c39418144
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-04-05 13:17:47 +0545

    program to download a web page with the correct character set.

--- src/QuestionTwentyOne.java
+++ src/QuestionTwentyOne.java
@@ -8,27 +8,40 @@ import java.net.URL;
 
 public class QuestionTwentyOne {
     public static void main(String[] args) {
-        try{
+        try {
             URL uri = new URL("https://www.example.com");
             HttpURLConnection conn = (HttpURLConnection) uri.openConnection();
             conn.setRequestMethod("GET");
             conn.connect();
+
             String charset = "UTF-8";
+
             String contentType = conn.getContentType();
-            for(String param : contentType.replace("", "").split(";")){
-                if(param.startsWith("charset = ")){
-                    charset = param.split("=", 2)[1];
-                    break;
+
+            if (contentType != null) {
+                for (String param : contentType.split(";")) {
+                    param = param.trim();
+                    if (param.startsWith("charset=")) {
+                        charset = param.split("=", 2)[1];
+                        break;
+                    }
                 }
             }
-            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), charset));
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream(), charset)
+            );
+
             String inputLine;
-            while((inputLine = in.readLine()) != null){
-                in.close();
-                conn.disconnect();
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
             }
+
+            in.close();
+            conn.disconnect();
+
         } catch (IOException e) {
-           e.printStackTrace();
+            e.printStackTrace();
         }
     }
 }
